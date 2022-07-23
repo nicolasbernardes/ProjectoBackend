2a333d88ba49d4d590d4a8fc9781fe8bcc28b20c
nicolasbernardes <nicolas.beltrao69@gmail.com>
2022-07-22 22:48:16 -0300

    heroku error

---
+++ README.md
@@ -0,0 +1,17 @@
+# E-commerce
+
+## Admin Login
+
+email: admin@example.com
+password: 123456
+
+
+## User Test Login 
+email: user@example.com
+password: 123456
+
+
+
+# test 3 add to heroku 
+
+
---
+++ backend/data.js
@@ -0,0 +1,73 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+  users: [
+    {
+      name: 'nicolas',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'teste',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      
+      name:'product 1',
+      slug:'product-1',
+      category:'category 1',
+      image:'/images/p1.jpg',
+      price: 100,
+      countInStock:10,
+      brand:'brand 1',
+      rating:3,
+      numReviews:10,
+      description:'description 1',
+    },
+    {
+      
+      name:'product 2',
+      slug:'product-2',
+      category:'category 2',
+      image:'/images/p2.jpg',
+      price:1000,
+      countInStock:0,
+      brand:'brand 2',
+      rating:10,
+      numReviews:10,
+      description:'description 2',
+    },
+    {
+   
+      name:'product 3',
+      slug:'product-3',
+      category:'category 3',
+      image:'/images/p3.jpg',
+      price:10000,
+      countInStock:10,
+      brand:'brand 2',
+      rating:10,
+      numReviews:10,
+      description:'description 3',
+    },
+    {
+    
+      name:'product 4',
+      slug:'product-4',
+      category:'category 4',
+      image:'/images/p4.jpg',
+      price:100000,
+      countInStock:10,
+      brand:'brand 4',
+      rating:10,
+      numReviews:10,
+      description:'description 4',
+    },
+  ],
+};
+export default data;
